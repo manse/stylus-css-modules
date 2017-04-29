91fedcee49a77f7a5137996204f7aee2c3e762ba
Ryota Mannari <info@manse.jp>
2017-04-29 12:22:59 +0900

    add selector map

---
+++ test/selector-map.test.js
@@ -0,0 +1,17 @@
+var SelectorMap = require('../lib/selector-map');
+
+function createSelectorMap() {
+    return new SelectorMap(function(id) {
+        return '_' + id;
+    });
+}
+
+test('map', () => {
+    var map = createSelectorMap();
+    expect(map.map('.a,.a.b,#id,.a .b:after @media (){}', 'foo')).toBe('._0,._0._1,#id,._0 ._1:after @media (){}');
+    expect(map.map('.a', 'foo')).toBe('._0');
+    expect(map.map('.a', 'bar')).toBe('._2');
+    expect(map.mangleNames.foo.a).toBe('_0');
+    expect(map.mangleNames.foo.b).toBe('_1');
+    expect(map.mangleNames.bar.a).toBe('_2');
+});
